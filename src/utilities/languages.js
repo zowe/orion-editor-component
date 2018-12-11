71b0ef918305827b92531b9e185d6c35aa63f984
Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>
2018-12-11 16:35:10 +0000

    orion-editor-component: Keeping it local
    Signed-off-by: Colin-Stone <30794003+Colin-Stone@users.noreply.github.com>

---
+++ src/utilities/languages.js
@@ -0,0 +1,31 @@
+/**
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Copyright IBM Corporation 2018
+ */
+import jcl from './jcl';
+import rexx from './rexx';
+
+const jsonData = {
+    'orion.Rexx': {
+        id: 'text/rexxcontext',
+        extension: ['rexx'],
+        dsn: ['**REXX', '*REXX*'],
+        name: 'Rexx',
+        extends: 'text/plain',
+        definitions: rexx,
+    },
+    'orion.JCL': {
+        id: 'text/jclcontext',
+        extension: ['jcl'],
+        dsn: ['**JCL', '*JCL'],
+        name: 'JCL',
+        extends: 'text/plain',
+        definitions: jcl,
+    },
+};
+export { jsonData as default };
